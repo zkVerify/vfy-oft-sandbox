8963f5ee719031e45b86ab840cfe287203baa93b
andreanistico <andrea@horizenlabs.io>
2025-06-23 15:24:29 +0200

    add assertion in tests

--- scripts/runTestNativeToOft.ts
+++ scripts/runTestNativeToOft.ts
@@ -1,11 +1,28 @@
 import { Options } from "@layerzerolabs/lz-v2-utilities";
-import { Contract, Transaction, Wallet } from "ethers";
+import { BigNumber, Contract, Transaction, Wallet } from "ethers";
 import hre from "hardhat";
+import { expect } from 'chai'
+
+const wait = (ms: number) => new Promise(res => setTimeout(res, ms));
+
+async function getOftBalanceOnSecondNetwork(addr: string): Promise<number> {
+  const secondChain = new hre.ethers.providers.JsonRpcProvider(process.env.OTHER_CHAIN_RPC);
+  const secondWallet = new hre.ethers.Wallet(process.env.OTHER_CHAIN_PRIVATE_KEY, secondChain);
+
+  const OFT = await hre.ethers.getContractFactory("MyOFT");
+  const oft = await OFT.attach(process.env.OTHER_ADDRESS);
+
+  return await oft.connect(secondWallet).balanceOf(addr);
+}
 
 async function runTests()  {
   
     const amountToSend = hre.ethers.utils.parseEther(process.env.TEST_VALUE)
     const signer = (await hre.ethers.getSigners())[0]
+    
+    //get balance first
+    const oftBalanceBefore = await getOftBalanceOnSecondNetwork(signer.address);
+    console.log(`oft balance on second chain is ${oftBalanceBefore}`)
 
     //get contract on first (native) chain
     const NativeOFTAdapter = await hre.ethers.getContractFactory("MyNativeOFTAdapter");
@@ -38,6 +55,20 @@ async function runTests()  {
       .send(sendParam, [nativeFee, 0], signer.address, { value: msgValue })
 
     console.log(`tx id: ${tx.hash}`)
+    
+    //wait
+    console.log(`waiting 2 minutes for LayerZero to complete the flow`)
+    await wait(120000);
+    
+    //get balance first
+    const oftBalanceAfter = await getOftBalanceOnSecondNetwork(signer.address);
+    console.log(`oft balance on second chain now is ${oftBalanceAfter}`)
+
+    //assert
+    const diff = oftBalanceAfter - oftBalanceBefore;
+    expect(diff.toString()).eql(amountToSend.toString());
+
+    console.log(`test passed`);
   }
 
 runTests()
